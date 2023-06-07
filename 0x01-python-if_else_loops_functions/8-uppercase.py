75bdafa8597ba7fd2c13d537ac2531dace9a5519
IBruteDude <kamarf505@gmail.com>
2023-06-07 05:33:36 +0200

    Second day done Alhamdulillah

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,10 @@
 def uppercase(str):
     for i in str:
         if ord('a') <= ord(i) and ord(i) <= ord('z'):
-            print("{}".format(chr(ord(i) + ord('A') - ord('a'))), end='', flush=True)
+            print("{}".format(chr(ord(i) + 65 - 97)), end='', flush=True)
         else:
             print("{}".format(i), end='')
     print("")
+
+
+uppercase("das")
