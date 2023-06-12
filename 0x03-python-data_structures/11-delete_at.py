5f11b4cd6f62c3a323e12119c3cad1529801cf7a
IBruteDude <kamarf505@gmail.com>
2023-06-12 16:59:29 +0200

    Almost all done AlHamdulillah

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -9,9 +9,3 @@ def delete_at(my_list=[], idx=0):
     for i in range(idx + 1, len(new_list)):
         my_list.append(new_list[i])
     return my_list
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
