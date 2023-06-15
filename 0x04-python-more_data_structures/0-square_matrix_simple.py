4550a64995c636c9ef62b0307d9e6395ae57cf57
IBruteDude <kamarf505@gmail.com>
2023-06-15 04:59:34 +0200

    Done Alhamdulillah

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,4 @@
-def square_matrix_simple(matrix=[]):
+#!/usr/bin/python3
+def square_matrix_simple (matrix=[]):
+    new_matrix = list(map(lambda column : list(map(lambda e : e ** 2, column)), matrix))
+    return new_matrix
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,8 @@
+#!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    for i in range(len(a_dictionary)):
+        print(f"{list(a_dictionary.keys())[i]}: {list(a_dictionary.values())[i]}")
+
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print_sorted_dictionary(a_dictionary)
