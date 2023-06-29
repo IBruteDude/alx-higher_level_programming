231c88341b29d6ede1b493420f354a64b5c63941
IBruteDude <kamarf505@gmail.com>
2023-06-29 04:55:02 +0200

    All done Alhamdulillah

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
+from sys import stderr
 def safe_function(fct, *args):
     try:
         fct(*args)
     except Exception as e:
-        print(e)
+        print("Exception: {}".format(e), file=stderr)
 
 
-if __name__ == '__main__':
-    pass
+if __name__ == '__main__':    
+    def my_div(a, b):
+        return a / b
+
+    result = safe_function(my_div, 10, 2)
+    print("result of my_div: {}".format(result))
+
+    result = safe_function(my_div, 10, 0)
+    print("result of my_div: {}".format(result))
+
+
+    def print_list(my_list, len):
+        i = 0
+        while i < len:
+            print(my_list[i])
+            i += 1
+        return len
+
+    result = safe_function(print_list, [1, 2, 3, 4], 10)
+    print("result of print_list: {}".format(result))
