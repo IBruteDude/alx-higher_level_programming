2bdef439f628646315a2dc8d54e18eb6b178fb6a
IBruteDude <kamarf505@gmail.com>
2023-06-29 04:21:37 +0200

    All done Alhamdulillah

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -27,6 +27,6 @@ if __name__ == '__main__':
     my_l_2 = [2, 0, "H", 2, 7]
     result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
     print(result)
-    
+
     result = list_division([10, 2], [], 2)
     print(result)
