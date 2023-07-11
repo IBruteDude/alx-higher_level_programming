8370ba15191f5e1470e6c26c94df07a946152b9f
IBruteDude <kamarf505@gmail.com>
2023-07-11 04:57:49 +0200

    Inheritence done, Alhamdulillah

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""A simple module for defining inverted equality int"""
+
+
+class MyInt(int):
+    """Inverted equality int class"""
+    def __eq__(self, other):
+        """Inverted equality"""
+        return int.__ne__(self, other)
+
+    def __ne__(self, other):
+        """Inverted inequality"""
+        return int.__eq__(self, other)
+
 
 if __name__ == '__main__':
-	pass
-
+	my_i = MyInt(3)
+	print(my_i)
+	print(my_i == 3)
+	print(my_i != 3)
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+def __setattr__(self, name, value):
 
 if __name__ == '__main__':
 	pass
