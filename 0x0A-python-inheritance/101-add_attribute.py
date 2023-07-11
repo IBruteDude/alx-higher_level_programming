d545757eefc4dc97f9c575619b3b38de3ddb0556
IBruteDude <kamarf505@gmail.com>
2023-07-11 04:59:55 +0200

    Inheritence done, Alhamdulillah

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,8 +2,7 @@
 def add_attribute(obj, name, value):
     if getattr(obj, name) is not None:
         raise TypeError("can't add new attribute")
-    else:
-        setattr(obj, name, value)
+    setattr(obj, name, value)
     
 
 if __name__ == '__main__':
