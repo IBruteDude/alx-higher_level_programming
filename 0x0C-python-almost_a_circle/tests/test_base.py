3e7cd43bc6a90dd061d9ee826c6236f638f8c8f0
IBruteDude <kamarf505@gmail.com>
2023-07-18 04:59:55 +0200

    Everything documented, Bismillah

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -5,23 +5,23 @@ from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
-class TestSquare(TestCase):
+class TestBase(TestCase):
     """Square instances unittesting class"""
     def setUp(self):
         """test units setup function"""
-        self.S1 = Square(5)
-        self.S2 = Square(10, 0, 0)
-        self.S3 = Square(6, 2, 3)
+        self.S1 = Base(5)
+        self.S2 = Base(10)
+        self.S3 = Base(6)
 
     def test_size(self):
         """Testing size property"""
-        self.assertEqual(self.S1.size, 5)
-        self.assertEqual(self.S2.size, 10)
-        self.assertEqual(self.S3.size, 6)
-        self.S1, self.S2, self.S3 = 20, 40, 60
-        self.assertEqual(self.S1, 20)
-        self.assertEqual(self.S2, 40)
-        self.assertEqual(self.S3, 60)
+        self.assertEqual(self.S1.id, 5)
+        self.assertEqual(self.S2.id, 10)
+        self.assertEqual(self.S3.id, 6)
+        self.S1.size, self.S2.size, self.S3.size = 20, 40, 60
+        self.assertEqual(self.S1.size, 20)
+        self.assertEqual(self.S2.size, 40)
+        self.assertEqual(self.S3.size, 60)
 
 if __name__ == '__main__':
     main()
