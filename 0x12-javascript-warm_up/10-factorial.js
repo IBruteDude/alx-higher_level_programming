3e4e6ce317448907ac7e96865fbed8b3cc6c7169
IBruteDude <kamarf505@gmail.com>
2023-09-12 02:06:23 +0000

    fixed styling

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-let number = parseInt(process.argv[2]);
+const number = parseInt(process.argv[2]);
 
-let fac = function fac(i) {
-	if (!(i > 0))
-		return 1;
-	return i * fac(i - 1);
-}
+const fac = function fac (i) {
+  if (!(i > 0)) { return 1; }
+  return i * fac(i - 1);
+};
 console.log(fac(number));
